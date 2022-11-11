3794867e476a23911f21fcee5001a7a296d7f691
YoungCoder <hungnv1@vnpay.vn>
2022-11-11 19:22:31 +0700

    push

---
+++ precache-manifest.6e8f200fa61e394dbeb8aab4fcddc9df.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf118e9323c88fbde5f20983ef131c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405fe1433cba7c8c8fcd",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "9101662905b564cdb89e",
+    "url": "/static/css/main.0171d668.chunk.css"
+  },
+  {
+    "revision": "405fe1433cba7c8c8fcd",
+    "url": "/static/js/2.dad06334.chunk.js"
+  },
+  {
+    "revision": "fa70aa7a2f67e20b85ea9b073b5979d4",
+    "url": "/static/js/2.dad06334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9101662905b564cdb89e",
+    "url": "/static/js/main.2d818727.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2d818727.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86795387024b75cdc913",
+    "url": "/static/js/runtime-main.f8089bb2.js"
+  },
+  {
+    "revision": "c0cabd33710ee4ba3603d21e5bf1de51",
+    "url": "/static/media/airplane.c0cabd33.jpg"
+  },
+  {
+    "revision": "182d144c5f945a6006b23e89e84409ba",
+    "url": "/static/media/deck_with_chicago.182d144c.jpg"
+  },
+  {
+    "revision": "0d262033cbb9527676ff2a9d89e4dbcb",
+    "url": "/static/media/header_large.0d262033.png"
+  },
+  {
+    "revision": "0d262033cbb9527676ff2a9d89e4dbcb",
+    "url": "/static/media/header_medium.0d262033.png"
+  },
+  {
+    "revision": "0d262033cbb9527676ff2a9d89e4dbcb",
+    "url": "/static/media/header_small.0d262033.png"
+  },
+  {
+    "revision": "ed06cd33edb800daedfe7d6fcf654110",
+    "url": "/static/media/orcas.ed06cd33.png"
+  },
+  {
+    "revision": "d80a434fce81655b439119411c73368f",
+    "url": "/static/media/sky_diving.d80a434f.jpeg"
+  },
+  {
+    "revision": "f704b8863bed68dab7d4f6e369a3e175",
+    "url": "/static/media/snow.f704b886.png"
+  },
+  {
+    "revision": "e126f67d80f0affb7a00db3bee858104",
+    "url": "/static/media/spain_octopus.e126f67d.jpg"
+  }
+]);
