410da4e9b76448c58f136ce37ef78ce85d260856
hung93.nguyenviet@gmail.com <hungnv1@vnpay.vn>
2022-11-21 22:50:39 +0700

    change 10

---
+++ precache-manifest.2810cdf2649e5115311be4dd4c9bb35b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1835e582cf7dd486e4a0e93da4098c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b1ac6bd55dda153761",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "8299d69a653359be4ac8",
+    "url": "/static/css/main.fa17a618.chunk.css"
+  },
+  {
+    "revision": "58b1ac6bd55dda153761",
+    "url": "/static/js/2.a589d2e2.chunk.js"
+  },
+  {
+    "revision": "fa70aa7a2f67e20b85ea9b073b5979d4",
+    "url": "/static/js/2.a589d2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8299d69a653359be4ac8",
+    "url": "/static/js/main.3173a5c0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3173a5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86795387024b75cdc913",
+    "url": "/static/js/runtime-main.f8089bb2.js"
+  },
+  {
+    "revision": "027c8373d344da5c04e87fcd560f6f88",
+    "url": "/static/media/airplane.027c8373.jpg"
+  },
+  {
+    "revision": "5fb7739c91d208323948ee2446044d62",
+    "url": "/static/media/deck_with_chicago.5fb7739c.jpg"
+  },
+  {
+    "revision": "4a6c25c3af15ae457f1d68d134dd8dd7",
+    "url": "/static/media/header_large.4a6c25c3.png"
+  },
+  {
+    "revision": "e21a397aff4a648c39ba5bf96d58f57c",
+    "url": "/static/media/header_medium.e21a397a.png"
+  },
+  {
+    "revision": "262f62bf6b8314d224cf1287d355bbbd",
+    "url": "/static/media/header_small.262f62bf.png"
+  },
+  {
+    "revision": "ed06cd33edb800daedfe7d6fcf654110",
+    "url": "/static/media/orcas.ed06cd33.png"
+  },
+  {
+    "revision": "d80a434fce81655b439119411c73368f",
+    "url": "/static/media/sky_diving.d80a434f.jpeg"
+  },
+  {
+    "revision": "f704b8863bed68dab7d4f6e369a3e175",
+    "url": "/static/media/snow.f704b886.png"
+  },
+  {
+    "revision": "48733d97b82e2b0395e57d62ad3da8d6",
+    "url": "/static/media/spain_octopus.48733d97.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.651318c317428d9fa6b7e10c0eec7e79.js"
+  "/precache-manifest.2810cdf2649e5115311be4dd4c9bb35b.js"
 );
 
 self.addEventListener('message', (event) => {
